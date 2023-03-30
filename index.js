0361972f75dfcf2332b39a5feb4dd527a0df1f24
+ <+>
2023-03-30 11:45:51 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+    for (let i = 0; i < array.length; i++) {
+      const complementNumber = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === complementNumber) return true;
+      }
+    }
+    return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
